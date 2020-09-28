5838c7b67deace331c48b7fb839afef71d064b94
cjwit <chris.witulski@gmail.com>
2020-09-28 18:55:00 -0400

    Remove error by putting ex1 code in callback

--- scripts/audio.js
+++ scripts/audio.js
@@ -1,11 +1,7 @@
-import * as Tone from 'tone'
-
-console.log("audio.js running");
-// import * as Tone from 'tone';
+import * as Tone from 'tone';
 //
 // boxes
 //
-
 function createBoxes({ name, pattern }) {
   var boxes = document.createElement("div");
   boxes.classList.add("boxes");
@@ -33,16 +29,13 @@ function createBoxes({ name, pattern }) {
     boxes.appendChild(box);
   }
 
-  // .box-row-four {
-  //   width: calc(25% - #{$box-label-width}/4 - #{$box-margin-left});
-  // }
-  
-  
   return boxes;  
 }
 
+//
 // build loop example from a data object
-export function createLoopExample(tagId, loopExampleData) {
+//
+export function createLoopExample(tagId, loopExampleData, audioFunction) {
   var example = document.getElementById(tagId);
 
   // create elements
@@ -63,31 +56,42 @@ export function createLoopExample(tagId, loopExampleData) {
     example.appendChild(part);
   }
   
-  // connect audio for example 1
-  const drumSampler = new Tone.Sampler({
-    urls: {
-      A1: "hihat.mp3",
-      A2: "snare.mp3",
-      A3: "kick.mp3"
-    },
-    baseUrl: "https://raw.githubusercontent.com/Tonejs/audio/master/drum-samples/acoustic-kit/"
-  }).toDestination();
-
-  const highHatLoop = new Tone.Loop(time => {
-    drumSampler.triggerAttackRelease("A1", "8n", time);
-  }, "8n").start(0);
-
-  const snareLoop = new Tone.Loop(time => {
-    drumSampler.triggerAttackRelease("A2", "4n", time);
-  }, "1n").start("2n");
-
-  const kickLoop = new Tone.Loop(time => {
-    drumSampler.triggerAttackRelease("A3", "4n", time);
-  }, "1n").start(0);
-
   loopButton.addEventListener('click', async() => {
-    await Tone.start();
-    console.log('audio ready for ' + tagId);
-    Tone.Transport.start();
+
+    // if not playing
+    if (loopButton.innerText == "Play loop") {
+
+      // example 1 audio setup
+      const drumSampler = new Tone.Sampler({
+        urls: {
+          A1: "hihat.mp3",
+          A2: "snare.mp3",
+          A3: "kick.mp3"
+        },
+        baseUrl: "https://raw.githubusercontent.com/Tonejs/audio/master/drum-samples/acoustic-kit/"
+      }).toDestination();
+      
+      const highHatLoop = new Tone.Loop(time => {
+        drumSampler.triggerAttackRelease("A1", "8n", time);
+      }, "8n").start(0);
+      
+      const snareLoop = new Tone.Loop(time => {
+        drumSampler.triggerAttackRelease("A2", "4n", time);
+      }, "1n").start("2n");
+      
+      const kickLoop = new Tone.Loop(time => {
+        drumSampler.triggerAttackRelease("A3", "4n", time);
+      }, "1n").start(0);
+    
+      Tone.Transport.bpm.value = 108;
+    
+      // generic callback code
+      await Tone.start();
+      Tone.Transport.start();
+      loopButton.innerText = "Stop";
+    } else {
+      Tone.Transport.stop();
+      loopButton.innerText = "Play loop";
+    }
   });
 }
--- scripts/script.js
+++ scripts/script.js
@@ -1,25 +1,26 @@
+import * as Tone from 'tone';
 import { createLoopExample } from './audio.js';
 
 //
 // example 1
 //
+
 var example1Data = {
   title: "Example 1: a basic rock beat",
   parts: [
     {
       name: "High hat",
-      pattern: [1, 1, 1, 1, 1, 1, 1, 1]
+      pattern: [1, 1, 1, 1, 1, 1, 1, 1],
     },
     {
       name: "Snare",
-      pattern: [0, 0, 1, 0]
+      pattern: [0, 0, 1, 0],
     },
     {
       name: "Bass drum",
-      pattern: [1, 0, 0, 0]
+      pattern: [1, 0, 0, 0],
     }
   ]
 }
 
 createLoopExample("example1", example1Data);
-console.log("end");
