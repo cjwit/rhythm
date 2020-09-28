1192ae876b5851f833b302dc6ffe300bac16137d
cjwit <chris.witulski@gmail.com>
2020-09-28 14:59:14 -0400

    Basic loop works

--- scripts/audio.js
+++ scripts/audio.js
@@ -64,10 +64,30 @@ export function createLoopExample(tagId, loopExampleData) {
   }
   
   // connect audio for example 1
+  const drumSampler = new Tone.Sampler({
+    urls: {
+      A1: "hihat.mp3",
+      A2: "snare.mp3",
+      A3: "kick.mp3"
+    },
+    baseUrl: "https://raw.githubusercontent.com/Tonejs/audio/master/drum-samples/acoustic-kit/"
+  }).toDestination();
+
+  const highHatLoop = new Tone.Loop(time => {
+    drumSampler.triggerAttackRelease("A1", "8n", time);
+  }, "8n").start(0);
+
+  const snareLoop = new Tone.Loop(time => {
+    drumSampler.triggerAttackRelease("A2", "4n", time);
+  }, "1n").start("2n");
+
+  const kickLoop = new Tone.Loop(time => {
+    drumSampler.triggerAttackRelease("A3", "4n", time);
+  }, "1n").start(0);
+
   loopButton.addEventListener('click', async() => {
     await Tone.start();
     console.log('audio ready for ' + tagId);
-    const synth = new Tone.Synth().toDestination();
-    synth.triggerAttackRelease("C4", "8n");
+    Tone.Transport.start();
   });
 }
