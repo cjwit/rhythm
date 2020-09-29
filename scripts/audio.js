75c6d91fecf681e8e16d76694d521bdaceaafc10
cjwit <chris.witulski@gmail.com>
2020-09-29 13:45:07 -0400

    Move sampler function to script.js

--- scripts/audio.js
+++ scripts/audio.js
@@ -60,34 +60,11 @@ export function createLoopExample(tagId, loopExampleData, audioFunction) {
 
     // if not playing
     if (loopButton.innerText == "Play loop") {
-
-      // example 1 audio setup
-      const drumSampler = new Tone.Sampler({
-        urls: {
-          A1: "hihat.mp3",
-          A2: "snare.mp3",
-          A3: "kick.mp3"
-        },
-        baseUrl: "https://raw.githubusercontent.com/Tonejs/audio/master/drum-samples/acoustic-kit/"
-      }).toDestination();
-      
-      const highHatLoop = new Tone.Loop(time => {
-        drumSampler.triggerAttackRelease("A1", "8n", time);
-      }, "8n").start(0);
-      
-      const snareLoop = new Tone.Loop(time => {
-        drumSampler.triggerAttackRelease("A2", "4n", time);
-      }, "1n").start("2n");
-      
-      const kickLoop = new Tone.Loop(time => {
-        drumSampler.triggerAttackRelease("A3", "4n", time);
-      }, "1n").start(0);
-    
-      Tone.Transport.bpm.value = 108;
-    
-      // generic callback code
       await Tone.start();
-      Tone.Transport.start();
+      audioFunction();
+      
+      // generic callback code
+      Tone.Transport.start("+0.1");
       loopButton.innerText = "Stop";
     } else {
       Tone.Transport.stop();
--- scripts/script.js
+++ scripts/script.js
@@ -23,4 +23,30 @@ var example1Data = {
   ]
 }
 
-createLoopExample("example1", example1Data);
+function example1Sampler() {
+  // example 1 audio setup
+  const drumSampler = new Tone.Sampler({
+    urls: {
+      A1: "hihat.mp3",
+      A2: "snare.mp3",
+      A3: "kick.mp3"
+    },
+    baseUrl: "https://raw.githubusercontent.com/Tonejs/audio/master/drum-samples/acoustic-kit/"
+  }).toDestination();
+
+  const highHatLoop = new Tone.Loop(time => {
+    drumSampler.triggerAttackRelease("A1", "8n", time);
+  }, "8n").start(0);
+
+  const snareLoop = new Tone.Loop(time => {
+    drumSampler.triggerAttackRelease("A2", "4n", time);
+  }, "1n").start("2n");
+
+  const kickLoop = new Tone.Loop(time => {
+    drumSampler.triggerAttackRelease("A3", "4n", time);
+  }, "1n").start(0);
+
+  Tone.Transport.bpm.value = 108;
+}
+
+createLoopExample("example1", example1Data, example1Sampler);
