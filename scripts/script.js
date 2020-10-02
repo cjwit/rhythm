bc908d35016c822348d582cad5d704e87bcd0183
cjwit <chris.witulski@gmail.com>
2020-10-02 14:23:44 -0400

    Add headers for all examples

--- scripts/script.js
+++ scripts/script.js
@@ -59,6 +59,20 @@ switch (page) {
     examples.default.tag = "example17";
     createExampleHeader("example17", examples.default, drumSampler);
 
-    // 17
+  case "part4.html":
+    examples.default.tag = "example18";
+    createExampleHeader("example18", examples.default, drumSampler);
+    examples.default.tag = "example19";
+    createExampleHeader("example19", examples.default, drumSampler);
+    examples.default.tag = "example20";
+    createExampleHeader("example20", examples.default, drumSampler);
+    examples.default.tag = "example21";
+    createExampleHeader("example21", examples.default, drumSampler);
+    examples.default.tag = "example22";
+    createExampleHeader("example22", examples.default, drumSampler);
+    examples.default.tag = "example23";
+    createExampleHeader("example23", examples.default, drumSampler);
+    examples.default.tag = "example24";
+    createExampleHeader("example24", examples.default, drumSampler);
 
 }
